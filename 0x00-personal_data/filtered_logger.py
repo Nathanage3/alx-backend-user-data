6ab7b1e1a969bc1f39ca048a032ead2862396a6f
Nathanage3 <sisay954@gmail.com>
2024-05-16 16:25:06 +0300

    updates

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,16 +1,36 @@
 #!/usr/bin/env python3
 """
-  Module for 'filtered_logger'
+Module for 'filtered_logger'
 """
 
 import logging
 import re
 from typing import List
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
-    """Obfuscate specified fields in a log message."""
+def filter_datum(
+                fields: List[str], 
+                redaction: str,
+                message: str, 
+                separator: str
+                ) -> str:
+    """
+    Obfuscate specified fields in a log message.
+
+    Parameters:
+    fields (List[str]): Fields to obfuscate
+    redaction (str): Obfuscation string
+    message (str): Log message
+    separator (str): Separator character in log message
+
+    Returns:
+    str: Obfuscated log message
+    """
     pattern = f'({"|".join(fields)})=.*?(?={separator}|$)'
-    return re.sub(pattern, lambda m: m.group(0).split('=')[0] + '=' + redaction, message)
+    return re.sub(
+        pattern,
+        lambda m: m.group(0).split('=')[0] + '=' + redaction,
+        message
+    )
 
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class """
@@ -24,5 +44,28 @@ class RedactingFormatter(logging.Formatter):
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """
+        Format a log record to include obfuscation.
+
+        Parameters:
+        record (logging.LogRecord): Log record to be formatted
+
+        Returns:
+        str: Formatted log record with obfuscated fields
+        """
         original_message = super(RedactingFormatter, self).format(record)
-        return filter_datum(self.fields, self.REDACTION, original_message, self.SEPARATOR)
+        """
+        Filter out sensitive data from a log message.
+
+        Parameters:
+        fields (List[str]): Fields to obfuscate
+        redaction (str): Obfuscation string
+        message (str): Log message
+        separator (str): Separator character in log message
+
+        Returns:
+        str: Obfuscated log message
+        """
+        return filter_datum(
+            self.fields, self.REDACTION, original_message, self.SEPARATOR
+        )
