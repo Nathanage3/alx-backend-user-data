51eeef56afe9d2677cbc051059b430690d06f015
Nathanage3 <sisay954@gmail.com>
2024-05-29 23:11:34 +0300

    new update

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,51 +1,82 @@
 #!/usr/bin/env python3
-"""DB module
+"""DB module.
 """
-import bcrypt
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, tuple_
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import Session
 from sqlalchemy.orm.exc import NoResultFound
-from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.session import Session
 
 from user import Base, User
 
 
 class DB:
-    """DB class
+    """DB class.
     """
 
     def __init__(self) -> None:
-        """Initialize a new DB instance
+        """Initialize a new DB instance.
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
     def _session(self) -> Session:
-        """Memoized session object
+        """Memoized session object.
         """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
-    
-    def add_user(self, email: str, hashed_password: str ) -> User:
-        """Add a new user to the database"""
-        new_user = User(email=email, hashed_password=hashed_password)
-        self._session.add(new_user)
-        self._session.commit()
-        return new_user
-    
-    def find_user_by(self, **kwargs) -> User:
-        """Find a user by arbitrary keyword argument"""
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """Adds a new user to the database.
+        """
         try:
-            user = self._session.query(User).filter_by(**kwargs).one()
-            return user
-        except NoResultFound:
-            raise NoResultFound("No user found for the provided criteria.")
-        except InvalidRequestError:
-            raise InvalidRequestError("Invalid query arguments provided.")
+            new_user = User(email=email, hashed_password=hashed_password)
+            self._session.add(new_user)
+            self._session.commit()
+        except Exception:
+            self._session.rollback()
+            new_user = None
+        return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Finds a user based on a set of filters.
+        """
+        fields, values = [], []
+        for key, value in kwargs.items():
+            if hasattr(User, key):
+                fields.append(getattr(User, key))
+                values.append(value)
+            else:
+                raise InvalidRequestError()
+        result = self._session.query(User).filter(
+            tuple_(*fields).in_([tuple(values)])
+        ).first()
+        if result is None:
+            raise NoResultFound()
+        return result
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Updates a user based on the provided user_id.
+        """
+        user = self.find_user_by(id=user_id)
+        if user is None:
+            return
+        new_update = {k: v for k, v in kwargs.items() if hasattr(User, k)}
+
+        if not new_update:
+            raise ValueError()
+
+        try:
+            self._session.query(User).filter(User.id == user_id).update(new_update,
+                synchronize_session=False,
+            )
+            self._session.commit()
+        except Exception as e:
+            self._session.rollback()
+            raise e
--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python3
-"""User module
+"""The `user` model's module.
 """
-
-import sqlalchemy
-from sqlalchemy import Column, String, DateTime, Integer
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from uuid import uuid4
+
 
 Base = declarative_base()
 
 
 class User(Base):
-    """Representation of user"""
-    __tablename__ = 'users'
-    id = Column(Integer(), primary_key=True)
+    """Represents a record from the `user` table.
+    """
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(250), nullable=False)
-    reset_token = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
