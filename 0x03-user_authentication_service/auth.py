611ade1adf2e10b7b3f103a5cb9e60f699663011
Nathanage3 <sisay954@gmail.com>
2024-05-30 19:58:17 +0300

    new updates

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -67,16 +67,16 @@ class Auth:
     return session_id
 
   def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
-    """Generate a password reset token for a user.
-    """
-    user = None
-    if session_id is None:
-      return None
-    try:
-      user = self._db.find_user_by(email=email)
-    except NoResultFound:
-      user = None
-    return user
+        """Retrieves a user based on a given session ID.
+        """
+        user = None
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
+        return user
 
 
 
