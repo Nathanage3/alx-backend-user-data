5c9b5bf22336314ef911da152b4d81496569c1d1
Nathanage3 <sisay954@gmail.com>
2024-05-31 00:43:47 +0300

    new update

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """A simple flask app
 """
-from flask import abort, Flask, jsonify, request
+from flask import abort, Flask, jsonify, request, redirect
 
 from auth import Auth
 
@@ -44,6 +44,32 @@ def login() -> str:
     response = jsonify({"email": email, "message": "logged in"})
     response.set_cookie("session_id", session_id)
     return response
+
+@app.route("/sessions", methods=["DELETE"], strict_slashes=False)
+def logout() -> str:
+  """Delete /sessions
+  Return:
+        - Redirects to home route.
+  """
+  session_id = request.cookies.get("session_id")
+  user = AUTH.get_user_from_session_id(session_id)
+  if user is None:
+    abort(403)
+  AUTH.destroy_session(user.id)
+  return redirect("/")
+
+@app.route("/profile", methods=["GET"], strict_slashes=False)
+def profile() -> str:
+    """GET /profile
+    Return:
+        - The user's profile information.
+    """
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+    return jsonify({"email": user.email})
+
 
 
 if __name__ == "__main__":
